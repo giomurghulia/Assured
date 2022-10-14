9f97c3bda9121f7aaa371a38a5006c98ef1429b9
giomurghulia <48592227+giomurghulia@users.noreply.github.com>
2022-10-14 20:41:56 +0400

    First commit

---
+++ app/src/main/java/com/insurance/assured/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.insurance.assured.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.insurance.assured.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
