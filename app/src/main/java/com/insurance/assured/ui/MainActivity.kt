601f3a896cae5e1e3798ec04b8c7ecb4857361d4
manjaro <dimitri.jibuti1123@gmail.com>
2022-10-18 21:52:10 +0400

    added custom splash screen

--- app/src/main/java/com/insurance/assured/ui/MainActivity.kt
+++ app/src/main/java/com/insurance/assured/ui/MainActivity.kt
@@ -4,24 +4,29 @@ package com.insurance.assured.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.fragment.app.findFragment
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.insurance.assured.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        lifecycleScope.launch(Dispatchers.Default) {
-            delay(3000)
-            lifecycleScope.launch {
-                binding.splash.visibility = View.GONE
-                //binding.tt.layoutParams = ViewGroup.LayoutParams(0,0)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                true
             }
         }
+        setContentView(binding.root)
+
     }
 }
